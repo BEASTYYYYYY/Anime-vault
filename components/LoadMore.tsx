d46266f7819204eed97fae2a6c4db32c5ab7b2cd
Yash kasera <yashkasera07@gmail.com>
2025-01-15 14:58:18 +0530

    Initial commit

---
+++ components/LoadMore.tsx
@@ -0,0 +1,51 @@
+"use client"
+
+import { fetchAnime } from "@/app/action";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import { useInView } from "react-intersection-observer";
+import AnimeCard from "./AnimeCard";
+
+let page = 2;
+
+export type AnimeCard = JSX.Element;
+
+function LoadMore() {
+
+  const { ref, inView } = useInView({
+    threshold: 0,
+  });
+  const [Data, SetData] = useState<AnimeCard[]>([]);
+
+  useEffect(() => {
+
+    if (inView) {
+      fetchAnime(page).then((res) => {
+        SetData([...Data, ...res]);
+        page++;
+      })
+    }
+
+  }, [inView, Data]);
+
+  return (
+    <>
+    <section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
+        {Data}
+      </section>
+      <section className="flex justify-center items-center w-full">
+        <div ref = {ref}>
+          <Image
+            src="./spinner.svg"
+            alt="spinner"
+            width={56}
+            height={56}
+            className="object-contain"
+          />
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default LoadMore;
---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['shikimori.one'], // Add the hostname here
+  },
+};
+
+module.exports = nextConfig;
